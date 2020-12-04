7cf2c42598f73db63546f24805419623a23f965a
Rachu Acharya <073bex429.rachu@pcampus.edu.np>
2020-12-04 20:27:39 +0545

    Description Json

--- create_desc_json.py
+++ create_desc_json.py
@@ -1,59 +1,80 @@
-"""
-Use this script to create JSON-Line description files that can be used to
-train deep-speech models through this library.
-This works with data directories that are organized like LibriSpeech:
-data_directory/group/speaker/[file_id1.wav, file_id2.wav, ...,
-                              speaker.trans.txt]
-
-Where speaker.trans.txt has in each line, file_id transcription
-"""
-
-from __future__ import absolute_import, division, print_function
-
-import argparse
+# Importing Dependencies 
+import csv
+import os 
+import re
 import json
-import os
 import wave
+from sys import argv, exit
 
+# Function to calculate duration of audio file
+def get_audio_duration(audio_path):
+    audio = wave.open(audio_path)
+    duration = float(audio.getnframes()) / audio.getframerate()
+    audio.close()
+    return duration
 
+# Main Function 
 def main(data_directory, output_file):
-    labels = []
-    durations = []
-    keys = []
-    for group in os.listdir(data_directory):
-        if group.startswith('.'):
-            continue
-        speaker_path = os.path.join(data_directory, group)
-        for speaker in os.listdir(speaker_path):
-            if speaker.startswith('.'):
-                continue
-            labels_file = os.path.join(speaker_path, speaker,
-                                       '{}-{}.trans.txt'
-                                       .format(group, speaker))
-            for line in open(labels_file):
-                split = line.strip().split()
-                file_id = split[0]
-                label = ' '.join(split[1:]).lower()
-                audio_file = os.path.join(speaker_path, speaker,
-                                          file_id) + '.wav'
-                audio = wave.open(audio_file)
-                duration = float(audio.getnframes()) / audio.getframerate()
-                audio.close()
-                keys.append(audio_file)
-                durations.append(duration)
-                labels.append(label)
+    # Key-words to drop line
+    numbers = ['१', '२', '३', '४', '५', '६', '७', '८', '९', '०']
+    # Open tsv file
+    transcript= open("./dev-clean/Nepali/utt_spk_text_small.tsv", "r")
+    transcript_reader=csv.reader(transcript, delimiter="\t" )
+    
+    file_names=list()
+    labels=list()
+
+    # Check and drop lines containing numbers
+    for row in transcript_reader:
+        drop_line=False
+        for letter in row[2]:
+            if letter in numbers:
+                drop_line=True
+                break
+        # If not dropped check for audio file 
+        if not drop_line:
+            file_names.append(row[0])
+            labels.append(row[2])
+
+    transcript.close()
+
+    # Bind file name and corresponding label
+    zipped_list = list(zip(file_names, labels))
+    sorted_zip = sorted(zipped_list, key = lambda x: x[0]) 
+
+    paths = list()
+    texts = list()
+    durations= list()
+
+    for file_name, label in sorted_zip:
+        # For file name beginning with 0-7, Audio_0_7 directory 
+        if re.match("(^[0-7])", file_name):
+            audio_path=os.path.join(data_directory, "Audio_0_7", file_name+".wav")
+            duration=get_audio_duration(audio_path)
+            paths.append(audio_path)
+            durations.append(duration)
+            texts.append(label)
+        # Else, directory Audio_8_f
+        else:
+            audio_path=os.path.join(data_directory,"Audio_8_f", file_name+".wav")
+            duration=get_audio_duration(audio_path)
+            paths.append(audio_path)
+            durations.append(duration)
+            texts.append(label)
+
+    # Dump contents as dictionary in json
     with open(output_file, 'w') as out_file:
-        for i in range(len(keys)):
-            line = json.dumps({'key': keys[i], 'duration': durations[i],
-                              'text': labels[i]})
+        for i in range(len(paths)):
+            line = json.dumps({'path': paths[i],'duration':durations[i],'text': texts[i]}, ensure_ascii=False)
             out_file.write(line + '\n')
 
+# Main function call
+if __name__ == "__main__":
+    if len(argv) !=3:
+        print("Error in command, exiting...")
+        exit()
+    data_directory = argv[1]
+    output_file = argv[2]
+    main(data_directory, output_file)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('data_directory', type=str,
-                        help='Path to data directory')
-    parser.add_argument('output_file', type=str,
-                        help='Path to output file')
-    args = parser.parse_args()
-    main(args.data_directory, args.output_file)
+    
